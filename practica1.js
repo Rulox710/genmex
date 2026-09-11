cdca24d7e880972af8ee45590b12ebd397c7051d
Raúl Nunova <rnunovaldes@ciencias.unam.mx>
2026-09-11 13:00:08 -0600

    Iniciando repo de practicas

---
+++ practica1.js
@@ -0,0 +1,11 @@
+class Main {
+  static main() {
+    console.log('Soy Raúl');
+    console.info('Estoy aprendiendo Javascript');
+    console.info('Quiero crear una página web');
+  }
+}
+
+Main.main();
+
+console.log('Ejecutando rama login');
